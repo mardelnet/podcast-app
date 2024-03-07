df5f811190f99eed118d3ea5620a93963d2efd5f
Pedro Figueroa <figueroash@gmail.com>
2024-03-07 11:19:23 +0100

    feat: add styles to the podcast page

--- src/pages/Podcast.tsx
+++ src/pages/Podcast.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { fetchSinglePodcast } from '../utils/fetchData';
 import { SinglePodcastType } from '../types/types';
+import styles from './Podcast.module.scss';
 
 // Extend PodcastParams from Record<string, string | undefined>
 interface PodcastParams extends Record<string, string | undefined> {
@@ -27,17 +28,41 @@ function Podcast() {
   }, [id]);
 
   return (
-    <div>
-      <h2>List of Tracks</h2>
-      {podcast && podcast.map((item: SinglePodcastType) => (
-        <p key={item.trackId}>
-          <Link 
-            state={{episodeUrl: item.episodeUrl}} 
-            to={`/podcast/${id}/episode/${item.trackId}`}>
-              {item.trackName}
-          </Link>
-        </p>
-      ))}
+    <div className='main-container'>
+      <div className={styles['podcast-container']}>
+        <div className={styles['podcast-item']}>aa</div>
+        <div className={styles['episodes']}>
+          <div className={styles['episodes__quantity']}>
+            Episodes: {podcast?.length}
+          </div>
+          <div className={styles['episodes__list']}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Date</th>
+                  <th>Duration</th>
+                </tr>
+              </thead>
+              <tbody>
+              {podcast && podcast.map((item: SinglePodcastType, index) => (
+                  <tr key={item.trackId} style={{ backgroundColor: index % 2 === 0 ? '#f6f6f6' : '#ffffff' }}>
+                    <td>
+                      <Link 
+                        state={{episodeUrl: item.episodeUrl}} 
+                        to={`/podcast/${id}/episode/${item.trackId}`}>
+                          {item.trackName}
+                      </Link>
+                    </td>
+                    <td>{item.releaseDate}</td>
+                    <td>{item.trackTimeMillis}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
